242cc0268a5afb1bad545f99c97bdfe79e22cd42
aysenrylmaz <58790145+aysenrylmaz@users.noreply.github.com>
2024-05-22 00:57:19 +0200

    Add files via upload

---
+++ loginpage.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginpage',
+  templateUrl: './loginpage.component.html',
+  styleUrls: ['./loginpage.component.css']
+})
+export class LoginpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    loginsecondPage(){
+if()
+
+    }
+
+
+  }
+
+}
+function loginsecondPage() {
+  throw new Error('Function not implemented.');
+}
+
